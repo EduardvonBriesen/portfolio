ec039a42ee961bbc0f88cd146cc4efca210724e0
EduardvonBriesen <e.v.briesen@googlemail.com>
2022-02-28 22:20:36 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
-// @ts-expect-error ts-migrate(6142) FIXME: Module './components/Hero' was resolved to '/home/... Remove this comment to see the full error message
 import Hero from './components/Hero';
 // import About from './About/About';
 // import Projects from './Projects/Projects';
-// @ts-expect-error ts-migrate(6142) FIXME: Module './components/Contact' was resolved to '/ho... Remove this comment to see the full error message
 import Contact from './components/Contact';
-// @ts-expect-error ts-migrate(6142) FIXME: Module './components/Footer' was resolved to '/hom... Remove this comment to see the full error message
 import Footer from './components/Footer';
 
 import { PortfolioProvider } from './context/context';
@@ -20,7 +17,6 @@ function App() {
 
   useEffect(() => {
     setAbout({ ...aboutData });
-    // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{ id: string; img: string; title... Remove this comment to see the full error message
     setProjects([...projectsData]);
     setContact({ ...contactData });
     setFooter({ ...footerData });
@@ -31,9 +27,7 @@ function App() {
       <Hero />
       {/* <About />
       <Projects /> */}
-
       <Contact />
-
       <Footer />
     </PortfolioProvider>
   );
